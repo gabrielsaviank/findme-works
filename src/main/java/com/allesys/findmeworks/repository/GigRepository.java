226296dd0b3c85c401a724d83c298897f84a76dc
Gabriel Savian <gabrielsvnzrd@gmail.com>
2024-03-18 10:01:49 -0300

    chore: security commit before changes

--- src/main/java/com/allesys/findmeworks/repository/GigRepository.java
+++ src/main/java/com/allesys/findmeworks/repository/GigRepository.java
@@ -1,4 +1,8 @@
 package com.allesys.findmeworks.repository;
 
-public interface GigRepository {
-}
+import com.allesys.findmeworks.entity.Gig;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface GigRepository extends JpaRepository<Gig, UUID> {}
